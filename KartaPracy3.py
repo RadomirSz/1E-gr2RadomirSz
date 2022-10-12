2d715b9575109dbdb9ca64a089300e5c3f0d73b3
RS31 <114557710+RadomirSz@users.noreply.github.com>
2022-10-12 06:29:49 +0000

    zad4,5,6

--- KartaPracy3.py
+++ KartaPracy3.py
@@ -9,7 +9,7 @@
 # #zad1
  # n = int(input())
  # for i in range(n):
- #   print(i**3+3, end)
+ #   print(i**3+3, end=" ")
 
 # print(list(range(10)))
 # print(list(range(5)))
@@ -37,11 +37,38 @@
 #for i in range(99,9,-1): print (i, end=" ")
 #pętla liczb 3-cyfrowych podzielnych przez 20
 # for i in range(100,981,20): print (i, end=" ")
-#zad2
-#for i in range(105,999,15): print(i, end=" ")
 
-#zad3
-a = int(input())
-for i in range(1,a+1):
- if (a % i == 0) :
-   print(i)
+#zad2
+#for i in range(105,1000,15): print(i, end=" ")
+
+# zad3
+# n = int(input())
+# for i in range(1,n+1):
+#  if (n % i == 0) :
+#    print(i, end=" ")
+
+#zad4
+# a = 0
+# for i in range(10,100):a = a + i;
+# print (a, end=" ")
+
+
+#zad5
+#ZAPAMIĘTAĆ
+# n = int(input())
+# suma = (n*(n+1)//2)
+
+# for i in range(n-1):
+#   x=int(input())
+#   suma = suma - x
+# print("nie podałeś:",suma)
+
+# zad6
+# n=int(input())
+# a = 1
+# b = 1
+# print (a, end=" ")
+# print (b, end=" ")
+# for i in range (n-1):
+#   a, b = b , a+b
+#   print(b, end=" ")
